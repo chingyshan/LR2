bec8baefbfc5cc40e534cd8403cfda3ec06b5679
chingyshan <vlad.ag070906@gmail.com>
2024-09-18 20:34:59 +0300

    V4

--- Task_6.cpp
+++ Task_6.cpp
@@ -2,22 +2,27 @@
 #include <cmath>
 using namespace std;
 int main() {
-    double x,D,B,p,n,C,Q,K,F;
+    double A,x,y,B,T,K,C,D,F;
+    unsigned int z;
     cout << "Введите значение x: "<<"\n";
     cin >> x;
-    cout << "Введите значение p: "<<"\n";
-    cin >> p;
-    cout << "Введите значение n: "<<"\n";
-    cin >> n;
+    cout << "Введите значение y: "<<"\n";
+    cin >> y;
+    cout << "Введите значение z: "<<"\n";
+    cin >> z;
     cout << "Введите значение D: "<<"\n";
     cin >> D;
     cout << "Введите значение K: "<<"\n";
     cin >> K;
-    B=cos(x);
-    C=p-n;
-    F=B*B/(K*D);
-    Q=F+(B*C*C*C);
-    cout << "Введите значение Q равно: "<<Q<<"\n";
+    cout << "Введите значение C: "<<"\n";
+    cin >> C;
+    A=x-y;
+    B=sqrt(z);
+    F=A*A/(K-(C*D));
+    T=cos(x)+F-B;
+
+
+    cout << "Введите значение Tравно: "<<T<<"\n";
 
 
 
